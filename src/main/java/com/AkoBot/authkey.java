c636aeda750500aeb8dc36f9398b4c398c2e4df7
Uninspirational <wooyeol1999@gmail.com>
2019-03-12 19:49:24 -0700

    removed key

--- src/main/java/com/AkoBot/authkey.java
+++ src/main/java/com/AkoBot/authkey.java
@@ -2,5 +2,5 @@ package com.AkoBot;
 
 
 public class authkey {
-    public static String key = "NDg3MTEwMTIxMTYwNDQxOTA1.D0TG_Q.tonlFY-Sud-sL4vbA-N6EHKnIDU"; //replace NO_KEY with discord authorization token
+    public static String key = "NO_KEY"; //replace NO_KEY with discord authorization token
 }
