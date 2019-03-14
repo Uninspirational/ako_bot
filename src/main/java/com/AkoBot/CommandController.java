e0493426d9e464e36ab47ba73a191f79fe9837d5
Uninspirational <wooyeol1999@gmail.com>
2019-03-14 02:45:00 -0700

    code cleanup + new bandorishuffle command

--- src/main/java/com/AkoBot/CommandController.java
+++ src/main/java/com/AkoBot/CommandController.java
@@ -47,8 +47,6 @@ public class CommandController {
 		}
 		//alert user of improper format
 		catch (IndexOutOfBoundsException e) {
-			textChannel.sendMessage("index out of bounds lul").queue();
-			e.printStackTrace();
 			incorrectCommandResponse(textChannel);
 		}
 	}
@@ -73,8 +71,24 @@ public class CommandController {
 		TextChannel textChannel = messageReceivedEvent.getTextChannel();
 		//String text = messageReceivedEvent.getMessage().getContentStripped();
 		switch (tag) {
+			case "$refreshall":
+				if (messageReceivedEvent.getAuthor().getId().equals("137710437490884608")) {
+					bandoriSongs = new BandoriRefreshCommand().bandoriRefresh(messageReceivedEvent);
+					bandoriMembers = new BandoriMembersLoadCommand().bandoriMembers(messageReceivedEvent);
+					bandoriCards = new BandoriCardLoadCommand().bandoriCards(messageReceivedEvent, bandoriMembers);
+				}
+				break;
+			case "$bangC":
+				if (messageReceivedEvent.getAuthor().getId().equals("137710437490884608"))
+					bandoriCards = new BandoriCardLoadCommand().bandoriCards(messageReceivedEvent, bandoriMembers);
+				break;
+			case "$bangMB":
+				if (messageReceivedEvent.getAuthor().getId().equals("137710437490884608"))
+					bandoriMembers = new BandoriMembersLoadCommand().bandoriMembers(messageReceivedEvent);
+				break;
 			case "$bandoriRefresh":
-				bandoriSongs = new BandoriRefreshCommand().bandoriRefresh(messageReceivedEvent);
+				if (messageReceivedEvent.getAuthor().getId().equals("137710437490884608"))
+					bandoriSongs = new BandoriRefreshCommand().bandoriRefresh(messageReceivedEvent);
 				break;
 			case "$help":
 				new HelpCommand().Help(messageReceivedEvent);
@@ -119,21 +133,15 @@ public class CommandController {
 			case "$bplay":
 				new BandoriPlayCommand().bplay(messageReceivedEvent, musicManager, bandoriSongs);
 				break;
-//			case "$bshuffle":
-//				bshuffle(messageReceivedEvent);
-//				break;
+			case "$bshuffle":
+				new BandoriShuffleCommand().bShuffle(messageReceivedEvent, musicManager, bandoriSongs);
+				break;
 			case "$bsearch":
 				new BandoriSongSearchCommand().searchBandoriSong(messageReceivedEvent, bandoriSongs);
-				break;
-			case "$bangC":
-				bandoriCards = new BandoriCardLoadCommand().bandoriCards(messageReceivedEvent);
 				break;
 			case "$card":
 			case "$c":
 				new BandoriCardCommand().bandoriCardCommand(messageReceivedEvent, bandoriCards, bandoriMembers);
-				break;
-			case "$bangMB":
-				bandoriMembers = new BandoriMembersLoadCommand().bandoriMembers(messageReceivedEvent);
 				break;
 			case "$member":
 			case "$m":
@@ -164,60 +172,4 @@ public class CommandController {
 	 * shuffles songs by a certain band
 	 * @param messageReceivedEvent messageReceivedEvent
 	 */
-//	private void bshuffle(MessageReceivedEvent messageReceivedEvent) {
-//		Date date = new Date();
-//		Random random = new Random();
-//		random.setSeed(date.getTime());
-//		TextChannel textChannel = messageReceivedEvent.getTextChannel();
-//		Member member = messageReceivedEvent.getMember();
-//		String[] keyword = messageReceivedEvent.getMessage().getContentStripped().split(" ");
-//		String band;
-//		boolean alpha = false;
-//		ArrayList<BandoriSong> sorted = new ArrayList<BandoriSong>();
-//		int i = 0, place;
-//		try {
-//			//shuffle all songs
-//			if (keyword[1].toLowerCase().equals("all")) {
-//				while (i < bandoriSongs.size()) {
-//					sorted.add(bandoriSongs.get(i++));
-//				}
-//				textChannel.sendMessage("I shuffled all songs randomly! Umm... Rin-rin! Teach me something cool to say here~").queue();
-//			}
-//			else {
-//				//shuffle songs by band
-//				if (keyword.length == 3 && keyword[2].equals("-a")) {
-//					alpha = true;
-//				}
-//				while (i < bandoriSongs.size()) {
-//					band = bandoriSongs.get(i).getBand();
-//					//System.out.println(keyword + " " + band + (band.toLowerCase().equals(keyword.toLowerCase()) || band.toLowerCase().contains(keyword.toLowerCase())));
-//					if (band.toLowerCase().contains(keyword[1].toLowerCase())) {
-//						sorted.add(bandoriSongs.get(i));
-//						//System.out.println(sorted.get(j).getName() + " " + sorted.get(j).getBand() + "RAN");
-//					}
-//					i++;
-//				}
-//				textChannel.sendMessage("I shuffled songs by " + (sorted.get(0).getBand().equals("Roselia") ? "...wait I'm in Roselia! " : sorted.get(0).getBand()) + ((alpha) ? "Also, the songs should play in alphabetical order now!" : "")).queue();
-//
-//			}
-//			//queue up chosen songs
-//			Join(messageReceivedEvent);
-//			for (i = 0; i < sorted.size(); i++) {
-//				if (alpha) {
-//					place = i;
-//					loadBandoriSong(textChannel, sorted.get(i), member);
-//				}
-//				else {
-//					place = random.nextInt(sorted.size());
-//					loadBandoriSong(textChannel, sorted.get(place), member);
-//				}
-////				System.out.println(sorted.get(place).getName() + " " + sorted.get(place).getBand());
-//				sorted.remove(place);
-//			}
-//		}
-//		catch (NullPointerException e) {
-//			e.printStackTrace();
-//			textChannel.sendMessage("I haven't practiced enough yet $bangM to give me some time!").queue();
-//		}
-//	}
 }
