2c43b95dfcee8fe911912dfa4db42b2d0862b218
Uninspirational <wooyeol1999@gmail.com>
2019-03-14 02:38:24 -0700

    added another parameter

--- src/main/java/com/AkoBot/Commands/BandoriCommands/BandoriCardLoadCommand.java
+++ src/main/java/com/AkoBot/Commands/BandoriCommands/BandoriCardLoadCommand.java
@@ -1,12 +1,13 @@
 package com.AkoBot.Commands.BandoriCommands;
 
 import com.AkoBot.Bandori.BandoriCards;
+import com.AkoBot.Bandori.BandoriMembers;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public class BandoriCardLoadCommand {
-    public BandoriCards bandoriCards(MessageReceivedEvent messageReceivedEvent) {
+    public BandoriCards bandoriCards(MessageReceivedEvent messageReceivedEvent, BandoriMembers bandoriMembers) {
         BandoriCards bandoriCards = new BandoriCards();
-        bandoriCards.bandoriRefresh(messageReceivedEvent);
+        bandoriCards.bandoriRefresh(messageReceivedEvent, bandoriMembers);
         return bandoriCards;
     }
 }
