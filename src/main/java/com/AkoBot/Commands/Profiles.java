7bdf80dab1e1e71d0bbbcab7442469cb5dbe640b
Uninspirational <wooyeol1999@gmail.com>
2019-04-09 06:44:45 -0700

    new class for game support

---
+++ src/main/java/com/AkoBot/Commands/Profiles.java
@@ -0,0 +1,26 @@
+package com.AkoBot.Commands;
+
+import java.util.ArrayList;
+
+public class Profiles {
+    private ArrayList<Profile> profiles;
+    public Profiles () {
+        this.profiles = new ArrayList<>();
+    }
+    public Profile getProfile(String id) {
+        for (Profile profile : this.profiles) {
+            if (profile.getUserId().equals(id)) {
+                return profile;
+            }
+        }
+        return new Profile(id);
+    }
+    public void addProfile(Profile profile) {
+        this.profiles.add(profile);
+    }
+    public void saveAll() {
+        for (Profile profile : this.profiles) {
+            profile.saveProfile();
+        }
+    }
+}
