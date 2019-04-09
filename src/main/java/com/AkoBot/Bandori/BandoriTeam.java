9bd9dee0fab574ec4f8a9b6d748ae6e5686500da
Uninspirational <wooyeol1999@gmail.com>
2019-04-09 07:05:49 -0700

    update

--- src/main/java/com/AkoBot/Bandori/BandoriTeam.java
+++ src/main/java/com/AkoBot/Bandori/BandoriTeam.java
@@ -8,14 +8,15 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 public class BandoriTeam {
-    ArrayList<BandoriCard> team;
+    private ArrayList<BandoriCard> team;
     public BandoriTeam() {
         this.team = new ArrayList<>();
     }
-    public void addCard(MessageReceivedEvent messageReceivedEvent, BandoriCard card, EventWaiter waiter) {
+    public void addCard(MessageReceivedEvent messageReceivedEvent, BandoriCards bandoriCards, EventWaiter waiter) {
         TextChannel textChannel = messageReceivedEvent.getTextChannel();
         String currentTeam = "";
         for (int i = 0; i < team.size(); i++) {
@@ -25,9 +26,9 @@ public class BandoriTeam {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.addField(currentTeam, "", false);
         textChannel.sendMessage(embedBuilder.build()).queue();
-        waitForAddCard(messageReceivedEvent.getAuthor(), textChannel, waiter);
+        waitForOldCard(messageReceivedEvent.getAuthor(), textChannel, waiter, bandoriCards);
     }
-    private void waitForAddCard(User user, TextChannel channel, EventWaiter waiter) {
+    private void waitForOldCard(User user, TextChannel channel, EventWaiter waiter, BandoriCards bandoriCards) {
         waiter.waitForEvent(GuildMessageReceivedEvent.class, e -> e.getChannel().equals(channel) && e.getAuthor().equals(user), e -> {
             TextChannel textChannel = e.getChannel();
             String message = e.getMessage().getContentStripped().toLowerCase();
@@ -38,13 +39,38 @@ public class BandoriTeam {
             try
             {
                 int oldCard = Integer.parseInt(message);
-
+                waitForNewCard(user, textChannel, waiter, oldCard, bandoriCards);
             }
             catch (Exception f) {
                 textChannel.sendMessage("Please enter the corresponding number for the card you would like to replace").queue();
-                this.waitForAddCard(user, channel, waiter);
+                this.waitForOldCard(user, channel, waiter, bandoriCards);
             }
         }, 25, TimeUnit.SECONDS, () -> channel.sendMessage("Timed out").queue());
 
+    }
+    private void waitForNewCard(User user, TextChannel channel, EventWaiter waiter, final int oldCard, BandoriCards bandoriCards) {
+        waiter.waitForEvent(GuildMessageReceivedEvent.class, e -> e.getChannel().equals(channel) && e.getAuthor().equals(user), e -> {
+            TextChannel textChannel = e.getChannel();
+            String message = e.getMessage().getContentStripped().toLowerCase();
+            if (message.equals("$cancel")) {
+                textChannel.sendMessage("Cancelled").queue();
+                return;
+            }
+            try
+            {
+                int newCard = Integer.parseInt(message);
+                replaceCard(textChannel, newCard, oldCard, bandoriCards);
+            }
+            catch (Exception f) {
+                textChannel.sendMessage("Please enter the corresponding number for the new card you would like to add").queue();
+                this.waitForNewCard(user, channel, waiter, oldCard, bandoriCards);
+            }
+        }, 25, TimeUnit.SECONDS, () -> channel.sendMessage("Timed out").queue());
+    }
+    private void replaceCard(TextChannel textChannel, int newCard, int oldCard, BandoriCards bandoriCards) {
+        BandoriCard[] cardsArray = (BandoriCard[]) this.team.toArray();
+        cardsArray[oldCard] = bandoriCards.getBandoriCards().get(newCard);
+        this.team.clear();
+        this.team.addAll(Arrays.asList(cardsArray));
     }
 }
