419cd2816893649f2ba7b95df3a853307fe1cffc
Uninspirational <wooyeol1999@gmail.com>
2019-04-09 06:43:56 -0700

    minor fixes

--- src/main/java/com/AkoBot/Commands/HelpCommands/Help2Command.java
+++ src/main/java/com/AkoBot/Commands/HelpCommands/Help2Command.java
@@ -20,7 +20,7 @@ public class Help2Command {
                                         "**$bplay <-all> <-type> <search term>** - find and play songs by a band or by song name\n" +
                                         "may include <-all> to play all songs that match search term\n" +
                                         "may include <-type> to specify type of song to play, replace \"type\" with a song type. See $help4 for more\n" +
-                                        "**$bshuffle <all** *or* **bandname> -type <song type>** - shuffle songs by a certain band or shuffle all songs alphabetically into the queue\n" +
+                                        "**$bshuffle <all** *or* **bandname> -type <song type>** - alphabetically shuffle songs by a certain band or shuffle all songs into the queue\n" +
                                         "include <all> to shuffle all songs *or* include a band name to shuffle all songs by that band\n" +
                                         "include -type <song type> to specify the type of song to shuffle. Defaults to either the game version or the first available version of the song. Don't include \"-type\" you don't want to specify song type\n" +
                                         "add -r at the end to randomize order\n" +
