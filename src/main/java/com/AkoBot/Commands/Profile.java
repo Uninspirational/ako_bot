a2ad8958fc7ca338d5ea804de88b6ea0ed64d336
Uninspirational <wooyeol1999@gmail.com>
2019-04-15 16:49:03 -0700

    update save and loading

--- src/main/java/com/AkoBot/Commands/Profile.java
+++ src/main/java/com/AkoBot/Commands/Profile.java
@@ -3,10 +3,10 @@ package com.AkoBot.Commands;
 import com.AkoBot.Bandori.BandoriCard;
 import com.AkoBot.Bandori.BandoriCards;
 import com.AkoBot.Bandori.BandoriTeam;
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Profile {
@@ -14,26 +14,34 @@ public class Profile {
     private ArrayList<BandoriCard> cards = null;
     private BandoriTeam team;
 
-    public Profile(String userId) {
+    public Profile(String userId, BandoriCards bandoriCards) {
         this.cards = new ArrayList<>();
         this.userId = userId;
+        this.team = new BandoriTeam();
+        loadSave(userId, bandoriCards);
     }
 
     public void addCards(BandoriCard bandoriCard) {
         this.cards.add(bandoriCard);
     }
 
+    public void addCardToTeam(MessageReceivedEvent messageReceivedEvent, EventWaiter waiter) {
+        this.team.addCardToTeam(messageReceivedEvent, cards, waiter);
+    }
     public boolean loadSave (String userId, BandoriCards bandoriCards) {
         try {
             String line;
             String id, xp, level;
-            if (new File(fileNameGetter()).exists()) {
-                RandomAccessFile raf = new RandomAccessFile(fileNameGetter(), "r");
-                while (!(line = raf.readLine()).equals("TEAM")) {
-                    id = line.substring(0, line.indexOf(" ") - 1);
-                    line = line.substring(line.indexOf(" "));
-                    xp = line.substring(0, line.indexOf(" ") - 1);
-                    line = line.substring(line.indexOf(" "));
+            if (new File(fileNameGetter(userId)).exists()) {
+                //RandomAccessFile raf = new RandomAccessFile(fileNameGetter(), "r");
+                File file = new File(fileNameGetter());
+                FileInputStream fr = new FileInputStream(file);
+                BufferedReader br = new BufferedReader(new InputStreamReader(fr, "UTF-8"));
+                while (((line = br.readLine()) != null && !line.equals("TEAM"))) {
+                    id = line.substring(0, line.indexOf(" "));
+                    line = line.substring(line.indexOf(" ") + 1);
+                    xp = line.substring(0, line.indexOf(" "));
+                    line = line.substring(line.indexOf(" ") + 1);
                     level = line;
                     for (BandoriCard card : bandoriCards.getBandoriCards()) {
                         if (card.getId() == Integer.parseInt(id)) {
@@ -43,11 +51,11 @@ public class Profile {
                         }
                     }
                 }
-                while ((line = raf.readLine()) != null) {
-                    id = line.substring(0, line.indexOf(" ") - 1);
-                    line = line.substring(line.indexOf(" "));
-                    xp = line.substring(0, line.indexOf(" ") - 1);
-                    line = line.substring(line.indexOf(" "));
+                while ((line = br.readLine()) != null) {
+                    id = line.substring(0, line.indexOf(" "));
+                    line = line.substring(line.indexOf(" ") + 1);
+                    xp = line.substring(0, line.indexOf(" "));
+                    line = line.substring(line.indexOf(" ") + 1);
                     level = line;
                     for (BandoriCard card : bandoriCards.getBandoriCards()) {
                         if (card.getId() == Integer.parseInt(id)) {
@@ -57,10 +65,14 @@ public class Profile {
                         }
                     }
                 }
+                br.close();
+                fr.close();
             }
+            else return false;
             return true;
         }
         catch (IOException e) {
+            e.printStackTrace();
             return false;
         }
     }
@@ -74,18 +86,28 @@ public class Profile {
 
     public void saveProfile() {
         try {
-            RandomAccessFile raf = new RandomAccessFile(fileNameGetter(), "w");
+            makeNewSaveFile();
+//            RandomAccessFile raf = new RandomAccessFile(fileNameGetter(), "rw");
+            File file = new File(fileNameGetter());
+            FileOutputStream fos = new FileOutputStream(file);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos, "UTF-8"));
             String save = "";
             for (BandoriCard bandoriCard : cards) {
                 save = save.concat(bandoriCard.getId() + " " + bandoriCard.getXp() + " " + bandoriCard.getLevel() + "\n");
             }
-            save = save.concat("TEAM");
-            for (BandoriCard bandoriCard: team.getTeam()) {
-                save = save.concat(bandoriCard.getId() + " " + bandoriCard.getXp() + " " + bandoriCard.getLevel() + "\n");
+            save = save.concat("TEAM\n");
+            if (team.getTeam() != null) {
+                for (BandoriCard bandoriCard : team.getTeam()) {
+                    if (bandoriCard != null)
+                        save = save.concat(bandoriCard.getId() + " " + bandoriCard.getXp() + " " + bandoriCard.getLevel() + "\n");
+                }
             }
-            raf.writeChars(save);
+            bw.write(save);
+            bw.close();
+            fos.close();
         }
         catch (IOException f) {
+            f.printStackTrace();
         }
     }
 
@@ -102,21 +124,23 @@ public class Profile {
     }
 
     private String fileNameGetter() {
+        return fileNameGetter(this.userId);
+    }
+
+    private String fileNameGetter(String userId) {
         String directory = "./src/main/resources/profiles/";
         return directory + userId + ".txt";
     }
-
-
     public ArrayList<BandoriCard> getCards() {
-        return cards;
+        return this.cards;
     }
 
     public BandoriTeam getTeam() {
-        return team;
+        return this.team;
     }
 
     public String getUserId() {
-        return userId;
+        return this.userId;
     }
 
 
